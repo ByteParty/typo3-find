255d40a5a13016119726c3a3639036269f9addf3
chrode <christof@rodejohann.de>
2024-07-29 17:49:09 +0200

    Raise version

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Find',
 	'description' => 'A frontend for Solr indexes',
-	'version' => '1.1.8',
+	'version' => '1.1.9',
 	'state' => 'stable',
 	'category' => 'frontend',
 	'shy' => 0,
